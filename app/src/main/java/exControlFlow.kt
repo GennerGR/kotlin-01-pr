55c0dc39eae0511e05a3a19ebfddfce1ed846155
gennerGR <lopezitzepcgenneralexander@gmail.com>
2024-06-27 11:22:51 -0600

    nothing

--- app/src/main/java/exControlFlow.kt
+++ app/src/main/java/exControlFlow.kt
@@ -16,14 +16,14 @@ fun main () {
         }
         println(result)
     }
-    ex1()
+    //ex1()
 
     println("siguiente ejercicio aqui")
     println("---------------------------------------------------------------------------------------")
 
 //Tienes un programa que cuenta porciones de pizza hasta que quede una pizza entera con 8 porciones.
 //Refactorice este programa de dos maneras:
-//loop while and do-while
+//loop with : while and do-while
 
 
     fun ex2_1 () {
@@ -36,7 +36,7 @@ fun main () {
         // End refactoring here
         println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
     }
-    ex2_1()
+   // ex2_1()
 
     println("---------------------------------------")
 
@@ -51,8 +51,46 @@ fun main () {
 
         println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
     }
-    ex2_2()
+    //ex2_2()
+    println("----------------------------------------------------------------")
+
+    //Escriba un programa que simule el juego de  Fizzbuzz .
+    //Su tarea es imprimir números del 1 al 100 de forma incremental
+    //Cualquier número divisible por 3 y 5 debe reemplazarse con la palabra "fizzbuzz".
+
+    fun ex3 () {
+
+        for (numero in 1..100) {
+            when {
+                numero % 3 == 0 -> println("fizz")
+                numero % 5 == 0 -> println("buzz")
+                else -> println(numero)
+            }
+        }
+    }
+    //ex3()
+    println("----------------------------------------------------------------")
+
+    //You have a list of words. Use for and if to print only the words that start with the letter l.
+
+    fun ex4() {
+        val words : MutableList<String> = mutableListOf("dinosaur", "limousine", "magazine", "language")
+        // Write your code here
+        for (getWords in words) {
+            //println(getWords)
+
+            println(
+                getWords.startsWith("li")
+            )
+        }
+    }
+    ex4()
 }
+
+
+
+
+
 
 
 
