4d9cf4dce7fd4f2933ea518c8afac68f0eb2cd06
gennerGR <lopezitzepcgenneralexander@gmail.com>
2024-07-03 08:24:02 -0600

    nothing

--- app/src/main/java/functions.kt
+++ app/src/main/java/functions.kt
@@ -1,5 +1,15 @@
+// Importaciones
 import kotlin.math.PI
 
+// Var's
+
+var numberList = listOf(1 , -2 , 3 , -4 , 5 , -6)
+
+// Lambda
+
+var sum = {x : Int, y : Int -> x + y}
+
+// Function
 
 fun circleAreaR(radius: Int): Double {
     return PI * radius * radius
@@ -18,7 +28,7 @@ fun principal() {
     println(intervalInSeconds(1, 0, 1))
 }
 
-//Funciones lamba (funciones flecha)
+// Funciones lamba (funciones flecha)
 
 fun uppercaseString(text: String): String {
     return text.uppercase()
@@ -28,33 +38,62 @@ fun principalTwo () {
     println(uppercaseString("heLlo"))
 }
 
+
+// Pass to another function
+
+fun passToAnotherFunction () {
+
+    var positive = numberList.filter{x -> x > 0}
+    var negative = numberList.filter{x -> x < 0}
+    println("numeros postivos $positive numeros negativos $negative")
+
+}
+
+// Map
+
+val doubled = numberList.map{x -> x * 2}
+val tripled = numberList.map{x -> x *3}
+
+fun printMap () {
+    println(doubled)
+    println(tripled)
+}
+
+// Function types
+var upperCaseString: (String) -> String = {text -> text.uppercase()}
+
+// MAIN FUNCTION
+
 fun main() {
+
+    println(sum(5, 4))
+    println("--------------------------------------------")
+
     println(circleAreaR(2))
     println(circleArea(2))
+    println("--------------------------------------------")
 
     principal()
+    println("--------------------------------------------")
 
     principalTwo()
+    println("--------------------------------------------")
 
-    //funcion lambda / flecha
-    println("------------------------------")
+     //println({text : String, other : Int -> text.uppercase(); other} ("hola", 5))
 
-    //println({text : String, other : Int -> text.uppercase(); other} ("hola", 5))
+    var funcionfl = {text : String, numero : Int -> text.uppercase() + numero}
 
-    var funcionfl = {text : String, other : Int -> text.uppercase() + other}
-
-    println(funcionfl("hola ", 5))
+    println(funcionfl("text ", 5))
     println("--------------------------------------------")
 
     passToAnotherFunction()
-}
+    println("--------------------------------------------")
 
-fun passToAnotherFunction () {
-    var numeros = listOf(1 , -2 , 3 , -4 , 5 , -6)
+    printMap()
+    println("--------------------------------------------")
 
-    var positivos = numeros.filter{x -> x > 0}
-    var negativos = numeros.filter{x -> x < 0}
-    println("numeros postivos $positivos numeros negativos $negativos")
+
+    println(upperCaseString("text"))
 
 }
 
