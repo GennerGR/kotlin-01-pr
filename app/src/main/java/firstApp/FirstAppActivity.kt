af7098673df138ea3e78af379ff02c81148daae8
gennerGR <lopezitzepcgenneralexander@gmail.com>
2024-07-19 10:59:23 -0600

    nothing - firstAppActivity

--- app/src/main/java/firstApp/FirstAppActivity.kt
+++ app/src/main/java/firstApp/FirstAppActivity.kt
@@ -12,10 +12,10 @@ class FirstAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //Al arrancar la pantalla
     }
 }
+
+
+
