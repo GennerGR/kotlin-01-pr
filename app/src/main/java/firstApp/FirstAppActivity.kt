30f2ab14d664f45dca48b5dda6f4536fbcd37e96
gennerGR <lopezitzepcgenneralexander@gmail.com>
2024-07-28 21:52:04 -0600

    mini mini mini aplicacion - first

--- app/src/main/java/firstApp/FirstAppActivity.kt
+++ app/src/main/java/firstApp/FirstAppActivity.kt
@@ -1,13 +1,21 @@
 package firstApp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlin_1.R
+import firstApp.Result
 
 class FirstAppActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +27,17 @@ class FirstAppActivity : AppCompatActivity() {
             insets
         }
         //Al arrancar la pantalla
+        val btnName = findViewById<AppCompatButton>(R.id.btnName)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnName.setOnClickListener {
+            val name = etName.text.toString()
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, Result::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
+        }
     }
 }
 
