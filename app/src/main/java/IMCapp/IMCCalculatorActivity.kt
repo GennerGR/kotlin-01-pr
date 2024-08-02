cee3d6cf5ea454087cb8d0ae5166cf18d0cc1bbb
Genner <lopezitzepcgenneralexander@gmail.com>
2024-08-02 00:03:50 -0600

    Logica de botones Male y Female

--- app/src/main/java/IMCapp/IMCCalculatorActivity.kt
+++ app/src/main/java/IMCapp/IMCCalculatorActivity.kt
@@ -3,11 +3,19 @@ package IMCapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlin_1.R
 
 class IMCCalculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected:Boolean = true
+    private var isFemaleSelected:Boolean = false
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,5 +26,23 @@ class IMCCalculatorActivity : AppCompatActivity() {
             insets
         }
 
+        initComponents()
+        initListeners()
+
     }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener { setGenderColor(isMaleSelected) }
+        viewFemale.setOnClickListener { setGenderColor(isFemaleSelected)}
+    }
+
+    private fun setGenderColor(isViewSelected:Boolean) {
+
+    }
+
 }
